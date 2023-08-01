a46cddb702094e2aed85a3731b7b5a3aef00599e
xyz310 <90328208+xyz310@users.noreply.github.com>
2023-08-01 15:46:17 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x60b5c84d1b3c7e672a02ec5a7ea4348f9cd6a986";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
